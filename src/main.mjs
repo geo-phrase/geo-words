580a21d0f27f7224722824ade89520b8e69aab3b
Grzegorz Kućmierz <gkucmierz@gmail.com>
2022-09-19 14:26:36 +0200

    add GeoWords object

--- src/main.mjs
+++ src/main.mjs
@@ -13,13 +13,13 @@ const word2id = (() => {
 
 const id2word = id => wordlist[id];
 
-const words2number = words => {
+const words2num = words => {
   return words
     .map(word2id)
     .reduce((res, n) => res * SIZE + n, 0);
 };
 
-const number2words = number => {
+const num2words = number => {
   const res = [];
   while (number > 0) {
     const idx = number % SIZE;
@@ -32,8 +32,48 @@ const number2words = number => {
 // console.log(words2number(['adapt', 'announce', 'arch', 'armor']));
 // console.log(number2words(206468989023));
 
-export default {
-  list: wordlist,
+const hex2num = hex => {
+  hex = hex.replace(/^0x/, '');
+  return parseInt(hex, 16);
+};
+
+const num2hex = (num, prefix = '0x') => {
+  return `${prefix}${num.toString(16)}`;
+};
+
+const GeoWords = () => {
+  let num, lat, lon, bitsLen;
+
+  const recalc = setter => {
+    num = setter();
+    const coords = gn.number2coords(num);
+    lat = coords.latitude;
+    lon = coords.longitude;
+    bitsLen = coords.sigBits.length;
+  }
+
+  return {
+    set num(n) { recalc(() => n); },
+    set hex(hex) { reclc(() => hex2num(hex)); },
+    set bits(bits) { recalc(() => bits2num(bits)); },
+    set words(words) { recalc(() => words2num(words)); },
+    set latitude(latitude) { recalc(() => gn.coords2number(latitude, lon, bitsLen)); },
+    set longitude(longitude) { recalc(() => gn.coords2number(lat, longitude, bitsLen)); },
+
+    get num() { return num; },
+    get bitsLen() { return bitsLen; },
+    get hex() { return num2hex(num); },
+    get bits() { return gn.number2coords(num).sigBits; },
+    get words() { return num2words(num); },
+    get latitude() { return gn.number2coords(num).latitude; },
+    get longitude() { return gn.number2coords(num).longitude; },
+    get locDetails() { return gn.number2coords(num); },
+  };
+};
+
+export {
+  wordlist,
   word2id,
   id2word,
 };
+export default GeoWords;
